b4c16d82ace4177aaed4eb16255efd6da8501692
176971923@qq.com <176971923@qq.com>
2017-06-13 17:39:14 +0800

    commit

--- src/main/java/com/lhy/test/java8/chapter3/Demo30.java
+++ src/main/java/com/lhy/test/java8/chapter3/Demo30.java
@@ -1,7 +1,24 @@
 package com.lhy.test.java8.chapter3;
 
+import java.util.function.IntConsumer;
+
 /**
  * Created by Administrator on 2017/6/10.
  */
 public class Demo30 {
+    /**
+     * @FunctionalInterface interface IntConsumer
+     */
+    public void repeat(int repeatTimes, IntConsumer com){
+        for (int i=0;i<repeatTimes;i++){
+            com.accept(i); // 执行一个lambda表达式
+        }
+    }
+
+    public static void main(String a[]){
+        Demo30 demo30 = new Demo30();
+        demo30.repeat(3,(x)->{int j = x*2; System.out.println(j);});
+    }
+
+
 }
--- src/main/java/com/lhy/test/java8/chapter9/Demo90.java
+++ src/main/java/com/lhy/test/java8/chapter9/Demo90.java
@@ -1,7 +1,23 @@
 package com.lhy.test.java8.chapter9;
 
+import java.util.Objects;
+
 /**
  * Created by Administrator on 2017/6/10.
  */
 public class Demo90 {
+    public static void main(String a[]){
+        String str1 = null;
+        String str2 = "";
+        String str3 = null;
+        String str4 = "test";
+        System.out.println(Objects.isNull(str1)); //判断 null
+        System.out.println(Objects.isNull(str2));
+        System.out.println(Objects.equals(str1,str2)); //equals 比较
+        System.out.println(Objects.equals(str1,str3));
+        System.out.println(Objects.hashCode(str4));     //计算 hash
+        System.out.println(Objects.hash(str4,"hehe","gaga"));
+        System.out.println(Integer.compare(123,4));     //比较大小
+        System.out.println(Integer.compare(123,4666));
+    }
 }
