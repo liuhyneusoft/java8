069b4213bff2fc5dcd998986cb51182c40bca155
Ocean <176971923@qq.com>
2017-09-20 15:32:58 +0800

    null

---
+++ src/main/java/com/lhy/test/java8/chapter1/TestNull.java
@@ -0,0 +1,26 @@
+ 
+public class TestNull {
+    public static void main(String ax[]){
+        InTest a = () -> null;
+        System.out.println(a.getx()); // null
+
+
+        InTest d = new InTest() {
+            @Override
+            public String getx() {
+                return "aaaaaaaaaaa";
+            }
+        };
+        System.out.println(d.getx()); // null
+
+        InTest b = () -> "bbbbb";
+        System.out.println(b.getx()); // bbbbb
+
+        InTest c = null;
+        System.out.println(c.getx());// NullPointerException
+    }
+}
+//函数式接口
+interface InTest{
+    String getx();
+}
